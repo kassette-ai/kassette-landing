70c4a2fe55ecc35cafe00d3ff261f17277fedfa8
Jim Collins <jim.collins@digital.homeoffice.gov.uk>
2023-05-02 11:38:49 +0100

    Update LandingPage.vue
    Updated content

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -59,8 +59,8 @@
                     </div>
                 </div>
                 <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Workplace data to enrich your organisation</h1>
-                    <p class="mt-6 text-lg leading-8 text-gray-600">Workflow data, analytics data, ETL. Too much custom development. Move to Kassette to simplify your data.</p>
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Realtime data to enrich your organisation</h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-600">Kassette makes it easy to collect and send your process data to the teams that need it.</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
                         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
